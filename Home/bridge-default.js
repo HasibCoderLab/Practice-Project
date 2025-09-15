d0d0408193db741976a020d23176800d9ee28979
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-15 22:40:17 +0600

    create bridge-default.js

---
+++ Home/bridge-default.js
@@ -0,0 +1,4 @@
+/* <![CDATA[ */
+var QodeAdminAjax = { "ajaxurl": "https://btibd.com/wp-admin/admin-ajax.php" };
+var qodeGlobalVars = { "vars": { "qodeAddingToCartLabel": "Adding to Cart...", "page_scroll_amount_for_sticky": "" } };
+/* ]]> */
